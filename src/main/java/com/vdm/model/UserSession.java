5b298781e20c01f5d46f4c875be41a427ff89e89
Vex1re <ivanovmatvej4548@gmail.com>
2026-06-20 17:58:16 +0400

    Реализация ролевой модели доступа

---
+++ src/main/java/com/vdm/model/UserSession.java
@@ -0,0 +1,35 @@
+package com.vdm.model;
+
+public class UserSession {
+    private static UserSession instance;
+    private User currentUser;
+
+    private UserSession() {}
+
+    public static UserSession getInstance() {
+        if (instance == null) {
+            instance = new UserSession();
+        }
+        return instance;
+    }
+
+    public void setUser(User user) {
+        this.currentUser = user;
+    }
+
+    public User getUser() {
+        return currentUser;
+    }
+
+    public void clearSession() {
+        this.currentUser = null;
+    }
+
+    public boolean isLoggedIn() {
+        return currentUser != null;
+    }
+
+    public boolean isPrivileged() {
+        return isLoggedIn() && currentUser.getRoleId() == 1;
+    }
+}
