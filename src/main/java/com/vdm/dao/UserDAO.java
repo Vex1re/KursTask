0cfcac7f5928a40a20e15d5439e94624310372e0
Vex1re <ivanovmatvej4548@gmail.com>
2026-06-20 17:56:20 +0400

    Реализация параметризированных запросов

---
+++ src/main/java/com/vdm/dao/UserDAO.java
@@ -0,0 +1,41 @@
+package com.vdm.dao;
+
+import com.vdm.model.User;
+import com.vdm.util.DatabaseConnection;
+
+import java.sql.*;
+
+public class UserDAO {
+
+    public User findByLogin(String login) throws SQLException {
+        String query = "SELECT * FROM Users WHERE login = ?";
+        try (Connection conn = DatabaseConnection.getInstance().getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setString(1, login);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return new User(
+                    rs.getInt("id_user"),
+                    rs.getString("name"),
+                    rs.getString("login"),
+                    rs.getString("password"),
+                    rs.getInt("id_role")
+                );
+            }
+        }
+        return null;
+    }
+
+    public boolean registerUser(User user) throws SQLException {
+        String query = "INSERT INTO Users (id_user, name, login, password, id_role) VALUES (?, ?, ?, ?, ?)";
+        try (Connection conn = DatabaseConnection.getInstance().getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setInt(1, user.getId());
+            stmt.setString(2, user.getName());
+            stmt.setString(3, user.getLogin());
+            stmt.setString(4, user.getPassword());
+            stmt.setInt(5, user.getRoleId());
+            return stmt.executeUpdate() > 0;
+        }
+    }
+}
