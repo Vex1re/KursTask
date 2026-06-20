da4434610e5eeecd4a2a29fe29d64f4f5b227175
Vex1re <ivanovmatvej4548@gmail.com>
2026-06-20 21:47:05 +0400

    Исправление навигации, CRUD клиентов и интерфейса

--- src/main/java/com/vdm/controller/ClientController.java
+++ src/main/java/com/vdm/controller/ClientController.java
@@ -5,8 +5,7 @@ import com.vdm.model.Client;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import java.sql.SQLException;
 
@@ -14,13 +13,34 @@ public class ClientController {
     @FXML private TableView<Client> clientTable;
     @FXML private TableColumn<Client, String> nameColumn;
     @FXML private TableColumn<Client, String> phoneColumn;
+    @FXML private TableColumn<Client, String> addressColumn;
 
     private ClientDAO clientDAO = new ClientDAO();
+
+    @FXML
+    public void initialize() {
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        phoneColumn.setCellValueFactory(new PropertyValueFactory<>("phoneNumber"));
+        addressColumn.setCellValueFactory(new PropertyValueFactory<>("address"));
+        refreshTable();
+    }
+
+    private void refreshTable() {
+        try {
+            clientTable.setItems(FXCollections.observableArrayList(clientDAO.getAll()));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     @FXML
     public void handleBack(javafx.event.ActionEvent event) throws Exception {
         com.vdm.util.ViewUtils.loadView("/view/main-view.fxml", event);
     }
+
+    @FXML public void handleAdd() {  }
+    @FXML public void handleEdit() {  }
+    @FXML public void handleDelete() {  }
 
     @FXML
     public void handleCreatePurchase(javafx.event.ActionEvent event) throws Exception {
@@ -29,8 +49,12 @@ public class ClientController {
             return;
         }
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/purchase.fxml"));
-        com.vdm.util.ViewUtils.loadView("/view/purchase.fxml", event);
+        javafx.scene.Scene scene = new javafx.scene.Scene(loader.load());
         PurchaseController controller = loader.getController();
         controller.setClient(selectedClient);
+        
+        javafx.stage.Stage stage = (javafx.stage.Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
-
+}
--- src/main/java/com/vdm/controller/MainController.java
+++ src/main/java/com/vdm/controller/MainController.java
@@ -2,46 +2,42 @@ package com.vdm.controller;
 
 import com.vdm.model.UserSession;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
 
 public class MainController {
     @FXML private Button editHotelsButton;
     @FXML private Button editClientsButton;
+    @FXML private Button createPurchaseButton;
 
     @FXML
     public void initialize() {
         if (!UserSession.getInstance().isPrivileged()) {
-            editHotelsButton.setDisable(true); 
+            editHotelsButton.setDisable(true);
         }
-        
         if (!UserSession.getInstance().canManageClients()) {
             editClientsButton.setDisable(true);
         }
     }
 
     @FXML
-    public void handleLogout(javafx.event.ActionEvent event) throws Exception {
+    public void handleLogout(ActionEvent event) throws Exception {
         UserSession.getInstance().clearSession();
-        loadView("/view/auth.fxml", event);
+        com.vdm.util.ViewUtils.loadView("/view/auth.fxml", event);
     }
 
     @FXML
-    public void handleManageHotels(javafx.event.ActionEvent event) throws Exception {
-        loadView("/view/hotel.fxml", event);
+    public void handleManageHotels(ActionEvent event) throws Exception {
+        com.vdm.util.ViewUtils.loadView("/view/hotel.fxml", event);
     }
 
     @FXML
-    public void handleManageClients(javafx.event.ActionEvent event) throws Exception {
-        loadView("/view/client.fxml", event);
+    public void handleManageClients(ActionEvent event) throws Exception {
+        com.vdm.util.ViewUtils.loadView("/view/client.fxml", event);
     }
-
-    private void loadView(String fxml, javafx.event.ActionEvent event) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml));
-        javafx.scene.Scene scene = new javafx.scene.Scene(loader.load());
-        javafx.stage.Stage stage = (javafx.stage.Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.show();
+    
+    @FXML
+    public void handleCreatePurchase(ActionEvent event) throws Exception {
+        com.vdm.util.ViewUtils.loadView("/view/purchase.fxml", event);
     }
-    }
-
+}
